60a2c648e197deaa842953056ee0bdef8a8e7759
colarrot <149344508+colarrot@users.noreply.github.com>
2023-11-17 15:29:17 +0900

    Update Codekata_1.cs

--- 2023_11/Codekata_1.cs
+++ 2023_11/Codekata_1.cs
@@ -4,12 +4,12 @@
     {
         string answer = "";
 
-        if (num % 2 == 1)
+        if (num % 2 == 1) // 2로나누었을때 나머지가 1 = 홀수
         {
             answer = "Odd";
         }
 
-        else if (num % 2 == 0)
+        else if (num % 2 == 0) // 2로나누었을때 나머지가 0 = 홀수
         {
             answer = "Even";
         }
