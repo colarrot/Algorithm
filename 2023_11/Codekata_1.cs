81520a9d6d787b6a66229ef0957fccb149c33028
colarrot <gunho7935@gmail.com>
2023-11-17 15:28:11 +0900

    짝수와 홀수

---
+++ 2023_11/Codekata_1.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public string solution(int num)
+    {
+        string answer = "";
+
+        if (num % 2 == 1)
+        {
+            answer = "Odd";
+        }
+
+        else if (num % 2 == 0)
+        {
+            answer = "Even";
+        }
+        return answer;
+    }
+
+}
