ec38a851a86c6c62731bfd7eaf8143c5559953a6
colarrot <gunho7935@gmail.com>
2023-11-20 10:08:45 +0900

    짝수와 홀수

---
+++ 2023_11/Codekata_11.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public string solution(int num)
+    {
+        string answer = "";
+
+        if (num % 2 == 1)  // 2로나누었을때 나머지가 1 = 홀수
+        {
+            answer = "Odd";
+        }
+
+        else if (num % 2 == 0)  // 2로나누었을때 나머지가 0 = 홀수
+        {
+            answer = "Even";
+        }
+        return answer;
+    }
+
+}
