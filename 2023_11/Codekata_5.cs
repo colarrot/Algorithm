39fa80497c6c78ee739eb211025378ec0d61fb0e
colarrot <gunho7935@gmail.com>
2023-11-21 10:00:44 +0900

    숫자 비교하기

---
+++ 2023_11/Codekata_5.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int num1, int num2)
+    {
+        int answer = 0;
+
+        if (num1 == num2)
+        {
+            return answer = 1;
+        }
+        else
+        {
+            return answer = -1;
+        }
+    }
+}
