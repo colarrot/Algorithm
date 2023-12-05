a63fc9fe5442a5c57ce081abee3c0ea4f68dfc21
colarrot <149344508+colarrot@users.noreply.github.com>
2023-12-05 10:15:27 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.17 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cs
@@ -0,0 +1,8 @@
+using System;
+public class Solution {
+    public long solution(long n) 
+    {
+       long sqrt = (long)Math.Sqrt(n);
+        return (sqrt * sqrt == n) ? (sqrt + 1) * (sqrt + 1) : -1;
+    }
+}
